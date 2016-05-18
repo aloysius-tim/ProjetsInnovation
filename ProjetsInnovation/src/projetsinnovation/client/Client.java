bffa1ae732e4482ebb35c98144804a219c9f70e5
Timothy Keynes <tim@keynes.fr>
2016-05-18 10:19:01 +0200

    Javadoc

--- ProjetsInnovation/src/projetsinnovation/client/Client.java
+++ ProjetsInnovation/src/projetsinnovation/client/Client.java
@@ -7,75 +7,68 @@ package projetsinnovation.client;
 
 import projetsinnovation.common.Request;
 
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.lang.reflect.Method;
 import java.net.Socket;
 import projetsinnovation.common.Response;
 import projetsinnovation.common.model.*;
 import projetsinnovation.server.Speaker;
 
-/**
- * @author Sofiane & Tim
- */
-
-/**
- * @author Alain Defrance
- */
 public class Client {
-    private ObjectInputStream ois;
-    private ObjectOutputStream oos;
+    private BufferedReader ois;
+    private PrintWriter oos;
     private String name;
     private Socket server;
     
     public Client() {
-        this.connect("localhost", 6000);
+        this.connect("10.212.96.221", 2002);
     }
     
     private void connect(String host, Integer port) {
         try {
             this.server = new Socket(host, port);
-            ois = new ObjectInputStream(this.server.getInputStream());
-            oos = new ObjectOutputStream(this.server.getOutputStream());
+            ois = new BufferedReader( new InputStreamReader( server.getInputStream() ) );
+            oos = new PrintWriter(server.getOutputStream());
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    
-    public void call(String method) {
+
+    public void call(String request){
         try {
-            this.call(method, null);
+            int value=0;
+            String str = "";
+
+            // reads to the end of the stream
+            while((value = ois.read()) != -1) {
+                char c = (char)value;
+                str+=c;
+
+                System.out.print(c);
+
+                //System.out.println("+++++"+str+"++++++");
+                if (str.endsWith("</askName>\n")){
+                    //System.out.println("IN");
+                    oos.println("Tim");
+                }
+            }
+
+            Speaker.speak("_______________________________");
         } catch(Exception e) {
             e.printStackTrace();
         }
     }
-    
-    public void call(String method, Object[] args) {
-        try {
-            Request request = new Request(method, args);
-            Speaker.speakWithTime("Sending request : " + request);
-            Speaker.speak("_______________________________");
-            oos.writeObject(request);
-            Response response = (Response)ois.readObject();
-            Speaker.speakWithTime("Received response : " + response);
-            Speaker.speak("_______________________________");
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-    }
-    
-    private void getIdeas() {this.call("getIdeas");}
-    private void createIdea(Idea idea) {this.call("createIdea", new Object[]{idea});}
-    private void getTeam(Idea idea) {this.call("getTeam", new Object[]{idea});}
-    private void subscribeTo(Idea idea, Student student) {this.call("subscribeTo", new Object[]{idea,student});}
-    
+
     public static void main(String[] args) {
         Client client = new Client();
-        
-        Student s = new Student("John", "john@doe.com"); // Nouvel étudiant
-        Idea idea = new Idea("Je veux faire un nouveau Uber", Technologies.WEB, s, null); // Idée déjà existante
-        Idea idea2 = new Idea("Voici une nouvelle idée", Technologies.MOBILE, s, null); // Nouvelle idée
 
-        client.getTeam(idea);
+        String ajouterIdee="add/n" +
+                "idee originale/n" +
+                "mobile/n";
+        String getListIdea="getListIdea/n";
+        String getStudentsInterested="getStudentInterestIdea/n" +
+                "0/n";
+
+        client.call("TEST");
     }
 }
